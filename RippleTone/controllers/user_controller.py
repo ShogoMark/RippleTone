c17910bee3c80bc708c620865e8f05a35df018bb
ShogoMark <trigmark87@gmail.com>
2023-05-28 01:53:45 +0100

    new

--- RippleTone/controllers/user_controller.py
+++ RippleTone/controllers/user_controller.py
@@ -1,25 +1,37 @@
+#!/usr/bin/python3
+
 from flask import render_template
 from views import RegisterForm
+from passlib.hash import sha256_crypt
 
 # User registration action
 @app.route('/register.html', method=['GET', 'POST'])
 def signup():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        name = form.name.data
-        email = form.email.data
+        username = form.name.data
         password = sha256_crypt.encrypt(str(form.password.data))
 
-        #create cursor
-        cur = mysql.connection.cursor()
+        create_user(username, password)
 
-        cur.execute("INSERT INTO users(name, email, password) VALUES(%s, %s, %s, %s)", (name, email, password))
+        return redirect(url_for('index'))
+ 
 
-        #commit DB
-        mysql.connection.commit()
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get the username and password entered by the user
+        username = request.form.get('username')
+        password = request.form.get('password')
 
-        #close cursor
-        cur.close()
-        return render_template('register.html')
-    return render_template('register.html', form=form)
+        # Perform authentication logic (e.g., check if username and password are valid)
+        if check_login(username, password):
+            # Authentication successful
+            # Redirect the user to a protected page or perform other actions
+            return redirect(url_for('dashboard'))
+        else:
+            # Authentication failed
+            error_message = 'Invalid username or password'
 
+    # Render the login template
+    return render_template('user/login.html', error_message=error_message)
