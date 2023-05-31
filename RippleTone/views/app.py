0f9b3fafb3964b6168edf6198f8cb8c4de6201eb
segunPc <eeomo@yahoo.com>
2023-05-31 15:54:25 +0100

    add app.py to render html file

---
+++ RippleTone/views/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# imports
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/home')
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
