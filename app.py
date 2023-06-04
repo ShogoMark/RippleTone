7b651d6b756ddb9b7d1c3caaf009bebfd1b1cf2b
root <root@ShogoMark>
2023-06-04 03:36:51 +0100

    mvc

---
+++ app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('html_page_template.html')
+
+# User registration action
+#@app.route('/login', methods=['GET', 'POST'])
+#def signup():
+ #   form = RegisterForm(request.form)
+  #  if request.method == 'POST' and form.validate():
+   #     name = form.name.data
+    #    password = sha256_crypt.encrypt(str(form.password.data))
+
+     #   create_user(name, password)
+
+      #  return redirect(url_for('index')
+ 
+
+#@app.route('/login', methods=['GET', 'POST'])
+#def login():
+ #   error_message = None
+  #  if request.method == 'POST':
+        # Get the username and password entered by the user
+    #    name = request.form.get('name')
+     #   password = request.form.get('password')
+
+        # Perform authentication logic (e.g., check if username and password are valid)
+      #  if check_login(name, password):
+            # Authentication successful
+            # Redirect the user to a protected page or perform other actions
+       #     return redirect(url_for('dashboard'))
+       # else:
+            # Authentication failed
+        #    error_message = 'Invalid username or password'
+
+    # Render the login template
+   # return render_template('user/login.html', error_message=error_message)
+
+if __name__ == '__main__':
+    app.run()
