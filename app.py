63a15b068d515e55a368997253b23c71aa9cb941
root <root@ShogoMark>
2023-06-04 04:45:15 +0100

    forms

--- app.py
+++ app.py
@@ -9,37 +9,37 @@ def index():
     return render_template('home.html')
 
 # User registration action
-#@app.route('/login', methods=['GET', 'POST'])
-#def signup():
- #   form = RegisterForm(request.form)
-  #  if request.method == 'POST' and form.validate():
-   #     name = form.name.data
-    #    password = sha256_crypt.encrypt(str(form.password.data))
+@app.route('/sign_up', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        password = sha256_crypt.encrypt(str(form.password.data))
 
-     #   create_user(name, password)
+        create_user(name, password)
 
-      #  return redirect(url_for('index')
+    return redirect(url_for('index')
  
 
-#@app.route('/login', methods=['GET', 'POST'])
-#def login():
- #   error_message = None
-  #  if request.method == 'POST':
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error_message = None
+    if request.method == 'POST':
         # Get the username and password entered by the user
-    #    name = request.form.get('name')
-     #   password = request.form.get('password')
+        name = request.form.get('name')
+        password = request.form.get('password')
 
-        # Perform authentication logic (e.g., check if username and password are valid)
-      #  if check_login(name, password):
+         Perform authentication logic (e.g., check if username and password are valid)
+        if check_login(name, password):
             # Authentication successful
             # Redirect the user to a protected page or perform other actions
-       #     return redirect(url_for('dashboard'))
-       # else:
+            return redirect(url_for('dashboard'))
+        else:
             # Authentication failed
-        #    error_message = 'Invalid username or password'
+            error_message = 'Invalid username or password'
 
     # Render the login template
-   # return render_template('user/login.html', error_message=error_message)
+    return render_template('user/login.html', error_message=error_message)
 
 if __name__ == '__main__':
     app.run()
