f1c63c6178307bac88e40e628cd73fb6fd342e88
ShogoMark <trigmark87@gmail.com>
2023-06-16 21:44:51 +0100

    newcommit

--- models/user_model.py
+++ models/user_model.py
@@ -23,59 +23,59 @@ Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
 class RegisterForm(FlaskForm):
-    Firstname = StringField('Firstname', validators=[DataRequired()])
-    Lastname = StringField('Lastname', validators=[DataRequired()])
-    Username = StringField('Username', validators=[DataRequired()])
-    Password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('Password')])
+    firstname = StringField('firstname', validators=[DataRequired()])
+    lastname = StringField('lastname', validators=[DataRequired()])
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    confirm_password = PasswordField('confirm_password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     user_id = Column(String(36), unique=True, default=str(uuid.uuid4()))
-    Username = Column(String(100), unique=True)
-    Password = Column(String(100))
-    Country = Column(String(100))
-    Email = Column(String(100))
+    username = Column(String(100), unique=True)
+    password = Column(String(100))
+    country = Column(String(100))
+    email = Column(String(100))
 
-    def __init__(self, Firstname, Lastname, Username, Password, Country, Email):
+    def __init__(self, firstname, lastname, username, password, country, email):
         self.user_id = str(uuid.uuid4())
-        self.Firstname = Firstname
-        self.Lastname = Lastname
-        self.Username = Username
-        self.Password = Password
-        self.Country = Country
-        self.Email = Email
+        self.firstname = firstname
+        self.lastname = lastname
+        self.username = username
+        self.password = password
+        self.country = country
+        self.email = email
 
 class LoginForm(FlaskForm):
-    Username = StringField('Username', validators=[DataRequired()])
-    Password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
     submit = SubmitField('Log In')
 
 # Function to check login details
-def check_login(Username, Password):
+def check_login(username, password):
     # Create a session
     session = Session()
     
     # Query the user by username
-    user = session.query(User).filter(User.Username == Username).first()
+    user = session.query(User).filter(User.username == username).first()
     
     if user is None:
         return False  # User not found
     
-    if user.Password == Password:
+    if user.password == password:
         return True  # Login successful
     
     return False  # Incorrect password
 
 
-def create_user(Firstname, Lastname, Username, Password, Country, Email):
+def create_user(firstname, lastname, username, password, country, email):
     # Create a session
     session = Session()
 
     # Create a new User instance
-    user = User(Firstname=Firstname, Lastname=Lastname, Username=Username, Password=Password, Country=Country, Email=Email)
+    user = User(firstname=firstname, lastname=lastname, username=username, password=password, country=country, email=email)
 
     # Add the user to the session
     session.add(user)
